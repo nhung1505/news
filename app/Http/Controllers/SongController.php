f2bfccea872865537fed2b67d14d9f50bdba5c51
Nhung <nguyennhungcl1505@gmail.com>
2017-11-02 18:47:53 +0700

    up logo

--- app/Http/Controllers/SongController.php
+++ app/Http/Controllers/SongController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Song;
 
 class SongController extends Controller
@@ -14,15 +15,36 @@ class SongController extends Controller
     }
 
     public function upload(Request $request){
+        $this->validate($request,[
+            'name' => 'required|unique:songs,name|min:3|max:50',
+            'audio' => 'required|mimes:mp3,mpga',
+            'image' => 'mimes:jpeg,jpg,png,svg'
+
+        ]);
 
         $song = new Song();
-        $song->name=$request->input('name');
-        $song->rysic=$request->input('rysic');
-        $song->description=$request->input('description');
-        $song->image=$request->file('image')->store('image_songs');
 
+        $song->name = $request->input('name');
+
+        $song->lyric = $request->input('lyric');
+
+        $song->description = $request->input('description');
+        if($request->hasFile('image')){
+            $song->image = $request->file('image')->store('image_songs/' . auth()->id(),'public');
+        }
+
+        $name_audio = $request->file('audio')->getClientOriginalName();
+        $time_current_audio = time();
+        $extension_audio = $request->file('audio')->getClientOriginalExtension();
+        $file_name_audio = $name_audio.$time_current_audio. '.' .$extension_audio;
+        $song->audio = $request->file('audio')->storeAs('audio_songs/' . auth()->id(),$file_name_audio,'public');
+
+        $song->user_id = Auth::id();
         $song->save();
 
+
         return redirect()->back();
+
+
     }
 }
