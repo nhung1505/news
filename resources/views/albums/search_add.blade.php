26734e3c3acbc4a9a1983929544e1e30fcfbd059
mac duc luong <macluong.mdl@gmail.com>
2017-11-13 23:35:13 +0700

    ...

--- resources/views/albums/search_add.blade.php
+++ resources/views/albums/search_add.blade.php
@@ -8,13 +8,12 @@
             {{ csrf_field() }}
             <input type="hidden" id="song_id" name="song_id"/>
             <input type="hidden" id="album_id" value="{{$album->id}}"/>
-            <input class="form-control col-md-8" type="text" id="q" placeholder="search name song" >
-            <input class="btn btn-success col-md-0.5" type="submit" value="Add" >
+            <input class="form-control col-md-11" type="text" id="q" placeholder="search name song" >
+            <input class="btn btn-success col-md-1" type="submit" value="Add" >
         </form>
         <table class="table table-striped">
-            <h5>New songs have been added</h5>
             @if(!isset($songAdd))
-                <td class=" text-center">No songs added yet</td>
+                <td class=" text-center">Find your favorite song</td>
             @endif
             @if(isset($songAdd))
                 <tr>
@@ -23,7 +22,7 @@
                         <form method="post" action="{{route('album.removeSong',['id'=>$album->id])}}">
                             {{csrf_field()}}
                             <input type="hidden" name="songAdd_id" value="{{$songAdd->id}}">
-                            <button class ="glyphicon glyphicon-remove text-danger btn pull-right" type="submit" onclick=" return myFunction()" ></button>
+                            <button data-toggle="tooltip" title="do not add" class ="glyphicon glyphicon-remove text-danger btn pull-right" type="submit"  onclick=" return myFunction()" ></button>
                         </form>
                     </td>
                 </tr>
