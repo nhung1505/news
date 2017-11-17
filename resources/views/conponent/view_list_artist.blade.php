530727c05676fe224a09e2bdbf7c5f64afc3b373
Nhung <nguyennhungcl1505@gmail.com>
2017-11-17 15:30:48 +0700

    link

--- resources/views/conponent/view_list_artist.blade.php
+++ resources/views/conponent/view_list_artist.blade.php
@@ -11,25 +11,22 @@
             </div>
             <div class="col-xs-5 text-right ">
                 {{--{{route('album.create')}}--}}
-                <a href="artists/create" class="btn btn-default btn-lg">
+                <a href="{{route('artist.create')}}" class="btn btn-default btn-lg">
                     <span class="glyphicon glyphicon-plus"></span> {{__('label.Create')}}
                 </a>
             </div>
         </div>
         @if(count($artists)==0)
-            {{--{{route('album.create')}}--}}
-            <p>{{__('label.No Album')}}. {{__('label.Would you like to create a ')}}<a href=""> {{__('label.new album')}}</a> ?</p>
+            <p>{{__('label.No Album')}}. {{__('label.Would you like to create a ')}}<a href="{{route('album.create')}}"> {{__('label.new album')}}</a> ?</p>
         @else
             @foreach($artists as $artist)
                 <div class="col-md 12 text-center">
                     <div class="col-md-3 well">
                         <div class="img-rounded song-cover-img-large">
-                            {{--{{route('album.detail_album', $album->id)}}--}}
-                            <a class="pl-3" href=""><img   alt="Cinque Terre" width="304" height="236" src="{{asset('storage/'.$artist->image)}}"></a>
+                            <a class="pl-3" href="{{route('artist.detail', $artist->id)}}"><img   alt="Cinque Terre" width="304" height="236" src="{{asset('storage/'.$artist->image)}}"></a>
                         </div>
                         <p>
-                            {{--{{route('album.detail_album', $album->id)}}--}}
-                            <a href="">{{$artist->name}}</a>
+                            <a href="{{route('artist.detail', $artist->id)}}">{{$artist->name}}</a>
                         </p>
                     </div>
                 </div>
